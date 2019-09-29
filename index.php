bb7be73a1102e804edd320860c509082e1fd990a
senantiasa <aldikurniawanfn@gmail.com>
2019-09-29 18:09:11 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <link rel="" href="">
+    <script type="text/javascript" src=""></script>
+    <title>Coba Git Remote</title>
+</head>
+
+<body>
+    <h2>Hello Developer</h2>
+</body>
+
+</html>
